4b06cbf32627e88aa2882b503c253b0db958f3dd
Anirudh Jayakumar <jayakumar.anirudh@gmail.com>
2015-10-30 19:15:59 -0500

    adding new classes

--- distFS/src/main/thrift/CommandInterface.thrift
+++ distFS/src/main/thrift/CommandInterface.thrift
@@ -2,9 +2,10 @@ namespace java edu.uiuc.cs425
 
 typedef i32 int
 
-service MemberIntroducer{
+service CommandInterface{
    int JoinGroup();
    binary GetMembershipList();
    void ReceiveElectionMessage();
    void ReceiveCoordinationMessage();
 }
+
